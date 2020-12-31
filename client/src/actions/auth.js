3ab25f078c59f83ac9fdb133b7729144da4b8389
Unknown <1456411@johnabbottcollege.net>
2020-12-31 15:15:52 -0500

    Implemented dynamic navbar according to authentication status

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -90,4 +91,9 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout / Clear Profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
